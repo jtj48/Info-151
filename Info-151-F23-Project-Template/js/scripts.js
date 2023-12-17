d17363e0a0d35f6ccb4d417d68c2c02425adc7ad
jtj48 <jtj48@drexel.edu>
2023-12-17 12:59:02 -0500

    rearranged html elements

--- Info-151-F23-Project-Template/js/scripts.js
+++ Info-151-F23-Project-Template/js/scripts.js
@@ -1,5 +1,7 @@
 
-
+/* =============
+    Nav
+============= */
 var mainNav = document.querySelector("#mainNav");
 
 var burgerBtn = document.querySelector("#burger-container")
@@ -23,9 +25,12 @@ function showNav(){
 burgerBtn.addEventListener("click",showNav);
 
 
+/* =============
+    Sign In
+============= */
 
-var signUpBtn = document.querySelector("sign-up-btn")
-var signUpContainer =  document.querySelector("signUp")
+var signUpBtn = document.querySelector("#sign-up-btn")
+var signUpContainer =  document.querySelector("#signUp")
 
 function showSignUpModal(){
     signUpContainer.classList.add("show-sign-up-container");
