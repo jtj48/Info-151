c36a342dbaaf8e9985ce4a05e9a2c7d2e87c5e0d
jtj48 <jtj48@drexel.edu>
2023-12-16 19:44:32 -0500

    added sign up js

--- Info-151-F23-Project-Template/js/scripts.js
+++ Info-151-F23-Project-Template/js/scripts.js
@@ -4,11 +4,18 @@ var mainNav = document.querySelector("#mainNav");
 
 var burgerBtn = document.querySelector("#burger-container")
 
-
-
+var seeNav = false;
 
 function showNav(){
     console.log("button clicked")
+    if(seeNav === false){
+        mainNav.classList.add("showMainNav");
+        seeNav= true;
+    }
+    else{
+        mainNav.classList.remove("showMainNav");
+        seeNav = false;
+    }
     mainNav.classList.add("showMainNav");
 }
 
@@ -17,4 +24,12 @@ burgerBtn.addEventListener("click",showNav);
 
 
 
+var signInBtn = document.querySelector("#sign-up-btn")
+var signInContainer =  document.querySelector("#signUp")
 
+function showSignInModal(){
+    signInContainer.classList.add("show-sign-in-container");
+}
+
+
+signInBtn.addEventListener("click",showSignInModal);
